0c44c62f47028f0660cc5fa932d3614cbe9a8d63
raj-sharan <gs.sharan@gmail.com>
2016-06-18 17:04:07 +0530

    Device authentication integrated

--- blogging/config/routes.rb
+++ blogging/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+ 
+  devise_for :users
+  root to: 'posts#index'
   resources :posts do
     get 'title', on: :member
     patch 'title', to: 'posts#change', on: :member
